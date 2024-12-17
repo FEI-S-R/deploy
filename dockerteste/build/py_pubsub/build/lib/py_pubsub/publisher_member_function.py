bd447c7c770bea22bde54730844bf39782cb0624
joca2511 <143613939+joca2511@users.noreply.github.com>
2024-12-17 16:33:16 +0000

    adicionando numeracao ao talker

--- dockerteste/build/py_pubsub/build/lib/py_pubsub/publisher_member_function.py
+++ dockerteste/build/py_pubsub/build/lib/py_pubsub/publisher_member_function.py
@@ -22,6 +22,8 @@ class MinimalPublisher(Node):
 
     def __init__(self):
         super().__init__('minimal_publisher')
+        self.declare_parameter('number',0)
+        self.number = self.get_parameter('number').get_parameter_value().integer_value
         self.publisher_ = self.create_publisher(String, 'topic', 10)
         timer_period = 0.5  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
@@ -29,7 +31,7 @@ class MinimalPublisher(Node):
 
     def timer_callback(self):
         msg = String()
-        msg.data = 'Hello World: %d' % self.i
+        msg.data = 'talker%d Published: Hello World: %d' % (self.number, self.i)
         self.publisher_.publish(msg)
         self.get_logger().info('Publishing: "%s"' % msg.data)
         self.i += 1
